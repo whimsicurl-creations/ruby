6a1abe676ce99db4696a322d1234bf95ae6b5bdf
Bethany <blmeier@gmail.com>
2020-09-02 23:09:28 -0400

    refactor always three scripts

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -1,9 +1,9 @@
 print "Give me a number"
-given_number = gets
-number_one = given_number.to_i
+given_number = gets.to_i
+number_one = given_number
 number_one += 5
 number_one *= 2
 number_one -= 4
 number_one /= 2
-number_one -= given_number.to_i
+number_one -= given_number
 puts "The final number is #{number_one}."
---
+++ ruby_challenges/always_three_reduced.rb
@@ -0,0 +1,3 @@
+puts "Give me a number"
+given_number = gets.to_i
+puts 'The final number is ' + (((given_number + 5) * 2 - 4) / 2 - given_number).to_s
