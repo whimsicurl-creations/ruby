453331411b6f59cba5e93392a477010381fbdc07
Bethany <blmeier@gmail.com>
2020-09-02 21:36:55 -0400

    Ruby practice - if/else and cases

---
+++ ruby_challenges/case.rb
@@ -0,0 +1,35 @@
+fav_color = 'pink'
+
+case fav_color
+when 'red'
+    puts "Red like fire!"
+when 'orange'
+    puts "Orange like, well... an orange."
+when 'yellow'
+    puts "Yellow daffodils are so pretty in the spring!"
+when 'green'
+    puts "Have you been to the Emerald City in Oz?"
+when 'blue'
+    puts "Blue like the sky!"
+when 'purple'
+    puts "Purple plums are the tastiest."
+else
+    puts "Hmm, well I don't know what that color is!"
+end
+
+weather_outlook = 'monsoon'
+
+case weather_outlook
+when 'sunny'
+    puts "Make sure to grab your sunglasses and some sunscreen!"
+when 'cloudy'
+    puts "Clouds in the sky, but grab the sunglasses just in case."
+when 'foggy'
+    puts "Turn on your headlights when driving."
+when 'rainy'
+    puts "Do a little singin' in the rain (but grab your umbrella, too)."
+when 'snowy'
+    puts "Bundle up, grab your gloves, and make sure you have an ice scraper in your car (or better yet, curl up under a blanket at home with a cup of cocoa)."
+else
+    puts "Be prepared for any kind of weather (you must live in Cincinnati!)."
+end
